63ff7f6da99caa2c7b2cef77a4cf914682b57ee3
ChunHan08 <149086722+ChunHan08@users.noreply.github.com>
2024-07-30 17:44:43 +0000

    update

---
+++ questions.js
@@ -0,0 +1,24 @@
+let question = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Text Preprocessor",
+      "Hyper Tool Multi Language",
+    ]
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet",
+    ]
+  }
+]
